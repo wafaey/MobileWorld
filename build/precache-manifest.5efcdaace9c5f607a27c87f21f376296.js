ca8d4bf27f11020b11cd82f75ecf2a2d38908905
wafaey <omarwafaey54@gmail.com>
2020-07-14 04:45:19 +0200

    build

---
+++ build/precache-manifest.5efcdaace9c5f607a27c87f21f376296.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "842f9b2ab197346df6931b1945f2d258",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbcdd6399e7960429d2b",
+    "url": "/static/css/main.aa299752.chunk.css"
+  },
+  {
+    "revision": "949dace190ce3ea9cd53",
+    "url": "/static/js/2.d7ae6633.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d7ae6633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbcdd6399e7960429d2b",
+    "url": "/static/js/main.74d2f1af.chunk.js"
+  },
+  {
+    "revision": "efaf543e1a5389fbc536",
+    "url": "/static/js/runtime-main.0275a3ba.js"
+  },
+  {
+    "revision": "21ef5947a01af65cddd378d82fb11bc7",
+    "url": "/static/media/cart.21ef5947.jpg"
+  },
+  {
+    "revision": "fe4394239e81b60accc18c539f16df8f",
+    "url": "/static/media/cloud.fe439423.jpg"
+  },
+  {
+    "revision": "92a21e72b977b61f2d46295bb730e892",
+    "url": "/static/media/feedback.92a21e72.jpg"
+  },
+  {
+    "revision": "708662b604e7bac00b841d8ebdacd0b6",
+    "url": "/static/media/iphone-11pro1.708662b6.jpg"
+  },
+  {
+    "revision": "30844b03c2a6ff420f7ed18d185434d8",
+    "url": "/static/media/iphone-x1.30844b03.jpg"
+  },
+  {
+    "revision": "8d2804332dc923828909e082a033d425",
+    "url": "/static/media/iphone-xr1.8d280433.jpg"
+  },
+  {
+    "revision": "b6ea81ae6da191bd5fc29a3f1cf48bea",
+    "url": "/static/media/iphone.b6ea81ae.jpg"
+  },
+  {
+    "revision": "5495a46d119b902d2a298eeb417b2254",
+    "url": "/static/media/iphone7.5495a46d.jpg"
+  },
+  {
+    "revision": "064cf317c1f968f63ea4aaaa876e4069",
+    "url": "/static/media/iphone8.064cf317.jpg"
+  },
+  {
+    "revision": "20340b9bfa41a150dbbe555397045428",
+    "url": "/static/media/iphoneSE1.20340b9b.jpg"
+  },
+  {
+    "revision": "ceeafc27ec7e45b73ebb434c2a9f4d2e",
+    "url": "/static/media/iphoneX1.ceeafc27.jpg"
+  }
+]);
